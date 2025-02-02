c402c690052fabdcd25d19030891d162251b9547
Jimmy Cedeno <jimmy199947@gmail.com>
2025-02-02 12:08:29 -0500

    change for joda

--- src/app/page.js
+++ src/app/page.js
@@ -100,7 +100,7 @@ export default function Game() {
 
   return (
     <div className="game">
-      <h1>Testing auto deployment</h1>
+      <h1>Vicente es gay</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
